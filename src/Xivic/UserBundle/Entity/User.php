71428f3bd8d669fa8550ff34d9742570f452d921
shade <shade@rosebud>
2013-04-24 18:56:27 +0300

    override getRoles() method-> removed default role

--- src/Xivic/UserBundle/Entity/User.php
+++ src/Xivic/UserBundle/Entity/User.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace Xivic\UserBundle\Entity;
-
+use FOS\UserBundle\Model\UserInterface as UInterface;
 use FOS\UserBundle\Entity\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
 
@@ -21,7 +21,29 @@ class User extends BaseUser
     public function __construct()
     {
         parent::__construct();
-        $this->roles = array('ROLE_ADMIN');
+        $this->roles = array();
 
     }
+    public function addRole($role)
+    {
+       
+         if (!in_array($role, $this->roles, true)) {
+            $this->roles[] = $role;
+        }
+
+        return $this;
+    }
+    
+    public function getRoles()
+    {
+        $roles = $this->roles;
+
+        foreach ($this->getGroups() as $group) {
+            $roles = array_merge($roles, $group->getRoles());
+        }
+        
+
+        return array_unique($roles);
+    }
+
 }
